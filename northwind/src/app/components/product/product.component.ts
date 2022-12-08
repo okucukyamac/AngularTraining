c51d9c1dbe69e5b82d385c8e58d41e6fab60f084
Oğuzhan Küçükyamaç <okucukyamac@outlook.com>
2022-12-09 02:22:36 +0300

    test

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -12,7 +12,7 @@ export class ProductComponent {
   product2: any = { productId: 2, productName: "Çatal", categoryId: 1, unitPrice: 5 }
   product3: any = { productId: 3, productName: "Kaşık", categoryId: 1, unitPrice: 5 }
   product4: any = { productId: 4, productName: "Tabak", categoryId: 1, unitPrice: 5 }
-  product5: any = { productId: 5, productName: "Belirsiz", categoryId: 1, unitPrice: 5 }
+  product5: any = { productId: 10, productName: "Belirsiz", categoryId: 1, unitPrice: 5 }
   products = [this.product1, this.product2, this.product3, this.product4, this.product5]
 
 }
