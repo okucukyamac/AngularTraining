c2103269e33b6d4319cf237d40f357a0cce3c6d1
Oğuzhan Küçükyamaç <okucukyamac@outlook.com>
2022-12-09 02:19:35 +0300

    First cmt

---
+++ northwind/src/app/components/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+
+  product1: any = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 5 }
+  product2: any = { productId: 2, productName: "Çatal", categoryId: 1, unitPrice: 5 }
+  product3: any = { productId: 3, productName: "Kaşık", categoryId: 1, unitPrice: 5 }
+  product4: any = { productId: 4, productName: "Tabak", categoryId: 1, unitPrice: 5 }
+  product5: any = { productId: 5, productName: "Belirsiz", categoryId: 1, unitPrice: 5 }
+  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
+
+}
